e906c2174eec8e0d1d32474cff117ba309b87a04
gyrodope69 <royr91430@gmail.com>
2023-01-29 21:55:02 -0600

    '.'

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const fileUpload = require('express-fileupload');
 
 const cors = require('cors');
 const { doesNotThrow } = require('assert');
 
 var app = express();
-
+app.use(fileUpload());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,15 +25,30 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.post('/upload', function (req, res) {
+  let sampleFile;
+  let uploadPath;
 
-// app.get('/mergedownload', (req, res) => {
-//   filepix.img2PDF(
-//     (pages = './public/images/test'),
-//     (output = `./public/images/merged/MergedCertificates-${Date.now()}.pdf`)
-//   );
-//   const file = `./public/images/merged/MergedCertificates-${Date.now()}.pdf`;
-//   res.download(file);
-// });
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.file;
+  uploadPath = __dirname + '/public/templates/' + 't1.png';
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+  sampleFile.mv('../client/public/template/t1.png', function (err) {
+    if (err) throw err;
+
+    // res.send('File uploaded!');
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* FOR UPLOADING THE FILE */
+
 /* FOR DOWNLOAD OF THE CERTIFICATES WHICH STORE FILES IN SERVER */
 router.post('/downloads', (req, res) => {
   updateCanvas(req.body).then((buffer) => {
